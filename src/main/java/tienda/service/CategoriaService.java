ebaef4f99fff45d936b9461e20cd0d91270cfd7f
CarterNR <carter090604@gmail.com>
2023-06-22 16:52:07 -0600

    carga 5
    Cambios de Semana 5

---
+++ src/main/java/tienda/service/CategoriaService.java
@@ -0,0 +1,11 @@
+package tienda.service;
+
+import tienda.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    
+    // Se obtiene un listado de categorias en un List
+    public List<Categoria> getCategorias(boolean activos);
+ 
+}
